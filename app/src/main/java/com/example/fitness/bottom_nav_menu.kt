a619298f0d520c30f6595f3ec0273f3820d843a1
YashodharaSandeepani <it22327062@my.sliit.lk>
2024-07-09 15:47:45 +0530

    Feat : Full Project

---
+++ app/src/main/java/com/example/fitness/bottom_nav_menu.kt
@@ -0,0 +1,10 @@
+package com.example.fitness
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class bottom_nav_menu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
